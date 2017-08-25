91f30986aa4d53caca65bcb50fd755c68ee4d9ef
David Belnap <david.belnap6@gmail.com>
2017-08-25 15:16:23 -0600

    minor changes, centered buttons

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-san_francisco.src = 'images/san_francisco'
+// // $(document).ready( function() {
+// 	$('.btn-large').on('click', function() {
+// 		position: absolute;
+// 		animation: mymove 5s infinite;
+// 	})
+// // })
+
+// @keyframes mymove {
+//     50% {opacity: 0;}
+// }
+
+// san_francisco.src = 'images/san_francisco'
+
+/* var images = [
+'path/to/image.jpg',
+'path/to/image.png',
+'path/to/image.jpg'
+]
+
+
+/* THIS CAN BE USED FOR ALERTING THE USER THAT THEY GOT
+A MATCH >>>>   Materialize.toast('I am a toast!', 3000, 'rounded')*/
+// 'rounded' is the class I'm applying to the toast
+
